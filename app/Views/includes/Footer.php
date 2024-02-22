303b17710b327fe0a229cb6c7998076fb64aa014
Davies John Boco <57469769+djTenTen@users.noreply.github.com>
2024-02-22 15:04:48 +0800

    done repositioning header and footer

--- app/Views/includes/Footer.php
+++ app/Views/includes/Footer.php
@@ -1,4 +1,10 @@
     
+        <footer class="footer">
+            <div><a href="https://coreui.io">CoreUI </a><a href="https://coreui.io">Bootstrap Admin Template</a> © 2023 creativeLabs.</div>
+            <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/docs/">CoreUI UI Components</a></div>
+        </footer>
+    </div>
+
     <!-- CoreUI and necessary plugins-->
     <script src="<?= site_url(); ?>vendors/@coreui/coreui/js/coreui.bundle.min.js"></script>
     <script src="<?= site_url(); ?>vendors/simplebar/js/simplebar.min.js"></script>
