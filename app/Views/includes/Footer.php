19b92d0f43cce36ba88527103193f8f55ea18174
Davies John Boco <57469769+djTenTen@users.noreply.github.com>
2024-02-22 13:33:32 +0800

    coreui design template

---
+++ app/Views/includes/Footer.php
@@ -0,0 +1,14 @@
+    
+    <!-- CoreUI and necessary plugins-->
+    <script src="<?= site_url(); ?>vendors/@coreui/coreui/js/coreui.bundle.min.js"></script>
+    <script src="<?= site_url(); ?>vendors/simplebar/js/simplebar.min.js"></script>
+    <!-- Plugins and scripts required by this view-->
+    <script src="<?= site_url(); ?>vendors/chart.js/js/chart.min.js"></script>
+    <script src="<?= site_url(); ?>vendors/@coreui/chartjs/js/coreui-chartjs.js"></script>
+    <script src="<?= site_url(); ?>vendors/@coreui/utils/js/coreui-utils.js"></script>
+    <script src="<?= site_url(); ?>js/main.js"></script>
+    <script>
+    </script>
+
+</body>
+</html>
