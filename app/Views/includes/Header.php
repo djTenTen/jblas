c5c8eebc80ace707900167f3a1dfbc853d2bcd4f
Davies John Boco <57469769+djTenTen@users.noreply.github.com>
2024-02-22 14:55:45 +0800

    design template working

--- app/Views/includes/Header.php
+++ app/Views/includes/Header.php
@@ -6,35 +6,36 @@
     <title>Document</title>
     
 
-    <link rel="apple-touch-icon" sizes="57x57" href="<?= site_url(); ?>assets/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?= site_url(); ?>assets/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?= site_url(); ?>assets/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?= site_url(); ?>assets/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?= site_url(); ?>assets/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?= site_url(); ?>assets/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?= site_url(); ?>assets/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?= site_url(); ?>assets/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= site_url(); ?>assets/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="<?= site_url(); ?>assets/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= site_url(); ?>assets/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?= site_url(); ?>assets/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= site_url(); ?>assets/favicon/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url(); ?>assets/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?= base_url(); ?>assets/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url(); ?>assets/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url(); ?>assets/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url(); ?>assets/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url(); ?>assets/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url(); ?>assets/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?= base_url(); ?>assets/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url(); ?>assets/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?= base_url(); ?>assets/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url(); ?>assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?= base_url(); ?>assets/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url(); ?>assets/favicon/favicon-16x16.png">
     
-    <link rel="manifest" href="<?= site_url(); ?>assets/favicon/manifest.json">
+    <link rel="manifest" href="<?= base_url(); ?>assets/favicon/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="<?= site_url(); ?>assets/favicon/ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="<?= base_url(); ?>assets/favicon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
     <!-- Vendors styles-->
-    <link rel="stylesheet" href="<?= site_url(); ?>vendors/simplebar/css/simplebar.css">
-    <link rel="stylesheet" href="<?= site_url(); ?>css/vendors/simplebar.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>vendors/simplebar/css/simplebar.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>css/vendors/simplebar.css">
     <!-- Main styles for this application-->
-    <link href="<?= site_url(); ?>css/style.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>css/style.css" rel="stylesheet">
     <!-- We use those styles to show code examples, you should remove them in your application.-->
-    <link href="<?= site_url(); ?>css/examples.css" rel="stylesheet">
-    <link href="<?= site_url(); ?>vendors/@coreui/chartjs/css/coreui-chartjs.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>css/examples.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>vendors/@coreui/chartjs/css/coreui-chartjs.css" rel="stylesheet">
 
 
 
 </head>
 <body>
     
+<?= base_url(); ?>
