296823a21f9e1903a1e9424e3b5a29b76d49ab66
Davies John Boco <57469769+djTenTen@users.noreply.github.com>
2024-03-04 15:10:31 +0800

    sb admin pro migration

--- app/Controllers/DashController.php
+++ app/Controllers/DashController.php
@@ -4,18 +4,16 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\API\ResponseTrait;
 use CodeIgniter\HTTP\ResponseInterface;
-
+    
 class DashController extends BaseController{
 
     public function dashboard(){
 
         $data['title'] = 'Dashboard';
 
-        echo view('includes/Header', $data);
-        echo view('dashboard/Dashboard');
-        echo view('includes/Footer');
-        
-        
+        echo view('dashboard/Dashboard', $data);
+
+    
     }
 
 }
--- app/Views/index/Login.php
+++ app/Views/index/Login.php
@@ -3,24 +3,79 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link href="css/styles.css" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.png" />
+    <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.29.0/feather.min.js" crossorigin="anonymous"></script>
+
+
 </head>
-<body>
+<body class="bg-primary">
 
-    <h1>Login Page</h1>
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                <div class="container-xl px-4">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-5">
+                            <!-- Basic login form-->
+                            <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                <div class="card-header justify-content-center"><h3 class="fw-light my-4">Login</h3></div>
+                                <div class="card-body">
+                                    <!-- Login form-->
+                                    <form action="authenticate" method="post">
+                                        <!-- Form Group (email address)-->
+                                        <div class="mb-3">
+                                            <label class="small mb-1" for="inputEmailAddress">Email</label>
+                                            <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter email address" name="email" required/>
+                                        </div>
+                                        <!-- Form Group (password)-->
+                                        <div class="mb-3">
+                                            <label class="small mb-1" for="inputPassword">Password</label>
+                                            <input class="form-control" id="inputPassword" type="password" placeholder="Enter password" name="password" required/>
+                                        </div>
+                                        <!-- Form Group (remember password checkbox)-->
+                                        <div class="mb-3">
+                                            <div class="form-check">
+                                                <input class="form-check-input" id="rememberPasswordCheck" type="checkbox" value="" />
+                                                <label class="form-check-label" for="rememberPasswordCheck">Remember password</label>
+                                            </div>
+                                        </div>
+                                        <!-- Form Group (login box)-->
+                                        <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                            <a class="small" href="auth-password-basic.html">Forgot Password?</a>
+                                            <button class="btn btn-primary" type="submit">Login</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="card-footer text-center">
+                                    <div class="small"><a href="auth-register-basic.html">Need an account? Sign up!</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+        <div id="layoutAuthentication_footer">
+            <footer class="footer-admin mt-auto footer-dark">
+                <div class="container-xl px-4">
+                    <div class="row">
+                        <div class="col-md-6 small">Copyright &copy; Your Website 2021</div>
+                        <div class="col-md-6 text-md-end small">
+                            <a href="#!">Privacy Policy</a>
+                            &middot;
+                            <a href="#!">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
 
-    <form action="authenticate" method="post">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
 
-        <label for="">
-            <input type="email" name="email">
-        </label>
-        <label for="">
-            <input type="password" name="password">
-        </label>
-
-        <button type="submit">login</button>
-            
-    </form>
-    
 </body>
 </html>
