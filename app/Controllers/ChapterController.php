98fae90a43fc0bb6b67e81ff65b27060370a3b07
Davies John Boco <57469769+djTenTen@users.noreply.github.com>
2024-04-10 12:39:50 +0800

    Done I2 pdf design

--- app/Controllers/ChapterController.php
+++ app/Controllers/ChapterController.php
@@ -1258,9 +1258,7 @@ class ChapterController extends BaseController{
             case '2.7 I2':
 
                 $data['qdata'] = $this->c2model->getquestionsdata($code,$dc2tID);
-                echo view('includes/Header', $data);
-                echo view('chapter2/27I2', $data);
-                echo view('includes/Footer');
+                echo view('pdfc2/I2', $data);
                 break;
 
             case '2.8 J2':
