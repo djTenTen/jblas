498c78f3de1e4f01a37389bc399ff2c1f09c94fa
Davies John Boco <57469769+djTenTen@users.noreply.github.com>
2024-04-12 13:19:50 +0800

    done AA5B design

---
+++ app/Views/pdfc3/AA5B.php
@@ -0,0 +1,161 @@
+<?php
+
+
+// create new PDF document
+$pageLayout = array(21, 29.7);
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, 'A4', true, 'UTF-8', false);
+$pdf->setPrintFooter(false);
+
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('ApplAud');
+$pdf->SetTitle($code);
+$pdf->SetSubject('TCPDF');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+// set default header data
+//$pdf->SetHeaderData("headerdispatch.png", 65);
+
+// set header and footer fonts
+$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
+$pdf->SetMargins(25,10,15);   
+//$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP-60, PDF_MARGIN_RIGHT);
+//$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetHeaderMargin(0);   
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+$pdf->setPrintHeader(false);
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// set some language-dependent strings (optional)
+if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+    require_once(dirname(__FILE__).'/lang/eng.php');
+    $pdf->setLanguageArray($l);
+}
+
+
+// ---------------------------------------------------------
+// set font
+
+
+
+// add a page
+$pdf->AddPage('L');
+//$pdf->SetPageSize('A4');
+
+
+$html =  "
+    <style>
+        *{
+            font-family: 'Times New Roman', Times, serif;
+            font-size: 14px;
+        }
+        h3{
+            font-size: 16px;
+        }
+        .cent{
+            text-align: center;
+        }
+        .bo{
+            border: 1px solid black;
+        }
+        p,li{
+            text-align: justify;
+        }
+        .bb{
+            border-bottom: 1px solid black;
+            
+        }
+        .ind{
+            text-indent: 20px;
+        }
+    </style>
+";
+
+$html .= '
+<table>
+    <tr>
+        <td style="width: 60%;">
+            <table>
+                <tr><td class="bb">Client:</td></tr>
+                <tr><td></td></tr>
+                <tr><td class="bb">Period:</td></tr>
+            </table>
+        </td>
+        <td style="width: 40%;">
+            <table border="1">
+                <tr>
+                    <td>Prepared by: <br></td>
+                    <td>Date: <br></td>
+                </tr>
+                <tr>
+                    <td>Reviewed by: <br></td>
+                    <td>Date: <br></td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+';
+
+
+$html .= '
+<h3>MANAGEMENT LETTER WORKSHEET [INTERIM / FINAL AUDIT]</h3>
+<table border="1" class="cent">
+    <thead>
+        <tr >
+            <th style="width: 7%;">SchRef.</th>
+            <th style="width: 18.6%;">Issues Identified </th>
+            <th style="width: 18.6%;">Client’s Comments</th>
+            <th style="width: 18.6%;">Recommendations</th>
+            <th style="width: 18.6%;">To be Included in Management Letter YES / NO</th>
+            <th style="width: 18.6%;">Results of Follow up at Next Audit Visit</th>
+        </tr>
+    </thead>
+    <tbody>
+    ';
+    foreach($aa5b as $r){
+        $html .= '
+        <tr>
+            <td style="width: 7%;"><br><br>'.$r['reference'].'<br></td>
+            <td style="width: 18.6%;"><br><br>'.$r['issue'].'<br></td>
+            <td style="width: 18.6%;"><br><br>'.$r['comment'].'<br></td>
+            <td style="width: 18.6%;"><br><br>'.$r['recommendation'].'<br></td>
+            <td style="width: 18.6%;"><br><br>'.$r['yesno'].'<br></td>
+            <td style="width: 18.6%;"><br><br>'.$r['result'].'<br></td>
+        </tr>';
+    }
+$html .='
+    </tbody>
+</table>
+<p>This should cover weaknesses in the accounting system and control environment plus comments on the qualitative aspects of the financial statements and the appropriateness of the accounting policies and estimation techniques adopted by the client.</p>
+<p>All significant issues should be included in the management letter.  For other issues verbal communication is adequate.  If there are no significant issues then this can be confirmed in a “voluntary” management letter or alternatively, the letter of representation can note that a management letter is not necessary ~ note, however, that this is likely to be a rare occurrence when applying IFRS.</p>
+';
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+//$pdf->write1DBarcode($rdata['reservation_id'], 'S25+', '', '', '', 18, 0.4, $style, 'N');
+//$pdf->Write(0, $html, '', 0, 'J', true);
+$pdf->writeHTML($html, true, false,'J', false, '');
+$pdf->Output('stocktransfer.pdf','I');
+exit();
