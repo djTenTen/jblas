a85497a57ab4eb7ddae13eb287ee3d02f5586c88
djTenTen <daviesjohn234567@gmail.com>
2024-08-19 18:08:02 -0700

    workpaper model update: it check if the file ac10 exist then it will update the ac10 file

--- app/Models/WorkpaperModel.php
+++ app/Models/WorkpaperModel.php
@@ -679,12 +679,15 @@ class WorkpaperModel extends  Model {
                 //case '17'   : $refaut = ['clientID' => $req['client'], 'firmID' => $req['firm'], 'code' => 'AC10', 'type' => 'payrollcu']; break;
             }
             $balcu = $this->db->table($this->tblc1)->where($refaut)->get()->getRowArray();
-            // if($balcu['question'] != 0){
-            //     $cal = ['question' => $req['debit'][$i] - $req['credit'][$i]];
-            // }else{
-                $cal = ['question' => $balcu['question'] + ($req['debit'][$i] - $req['credit'][$i])];
-            //}
-            $this->db->table($this->tblc1)->where($refaut)->update($cal);
+            if(!empty($balcu)){
+                // if($balcu['question'] != 0){
+                //     $cal = ['question' => $req['debit'][$i] - $req['credit'][$i]];
+                // }else{
+                    $cal = ['question' => $balcu['question'] + ($req['debit'][$i] - $req['credit'][$i])];
+                //}
+                $this->db->table($this->tblc1)->where($refaut)->update($cal);
+            }
+            
             $this->db->table($this->tblcfi)->where(array('clientID' => $req['client'], 'firm' => $req['firm'], 'workpaper' => $req['workpaper'], 'index' => $index))->update(array('acquired' => 'Yes'));
             $data = [
                 'client'        => $req['client'],
