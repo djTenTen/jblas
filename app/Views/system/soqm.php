3f605475f014fefaab91e82538f3e317f1a7c98d
djTenTen <daviesjohn234567@gmail.com>
2025-01-11 02:52:31 -0700

    SOQM Manual update design

--- app/Views/system/soqm.php
+++ app/Views/system/soqm.php
@@ -44,30 +44,29 @@
             <div class="card-body">
                 <hr style="color: #7752FE;" class="m-3">
                 <div class="m-5">
-                    <?php if($soqm['soqm'] != 'Using' and $soqm['soqm'] != 'Uploaded'){?>
-                        <p>Here, you have the option to either use the default SOQM Manual provided by the system or upload your own customized version to suit your specific requirements.</p>
-                        <div class="container row">
-                            <div class="col-6">
-                                <h3>Use ApplAud's SOQM</h3>
-                                <form action="<?= base_url()?>auditsystem/soqm/use" method="post">.
-                                    <?php if($soqm['soqm'] != 'Using'){?>
-                                        <button type="submit" class="btn btn-primary m-1 btn-sm"><i class="fas fa-file-alt m-1"></i>Use SOQM</button>
-                                    <?php }?>
-                                    <a type="button" href="<?= base_url()?>auditsystem/soqmpdf" target="_blank" class="btn btn-success m-1 btn-sm"><i class="fas fa-eye m-1"></i>View SOQM Template</a>
-                                </form>
-                            </div>
-                            <div class="col-6">
-                                <h3>Upload your own SOQM</h3>
-                                <form action="/auditsystem/soqm/upload" enctype="multipart/form-data" method="post">
-                                    <div class="col-6">
-                                        <input type="file" name="soqmfile" accept=".pdf" class="form form-control btn btn-secondary">
-                                    </div>
-                                    <button type="submit" class="btn btn-primary m-1 btn-sm"><i class="fas fa-file-pdf m-1"></i>Upload</button>
-                                </form>
-                            </div>
+                    
+                    <p>Here, you have the option to either use the default SOQM Manual provided by the system or upload your own customized version to suit your specific requirements.</p>
+                    <div class="container row">
+                        <div class="col-6">
+                            <h3>Use ApplAud's SOQM</h3>
+                            <form action="<?= base_url()?>auditsystem/soqm/use" method="post">.
+                                    <button type="submit" class="btn btn-primary m-1 btn-sm"><i class="fas fa-file-alt m-1"></i>Use SOQM</button>
+                                <a type="button" href="<?= base_url()?>auditsystem/soqmpdf" target="_blank" class="btn btn-success m-1 btn-sm"><i class="fas fa-eye m-1"></i>View SOQM Template</a>
+                            </form>
                         </div>
-                    <?php }?>
+                        <div class="col-6">
+                            <h3>Upload your own SOQM</h3>
+                            <form action="/auditsystem/soqm/upload" enctype="multipart/form-data" method="post">
+                                <div class="col-6">
+                                    <input type="file" name="soqmfile" accept=".pdf" class="form form-control btn btn-secondary" required>
+                                </div>
+                                <button type="submit" class="btn btn-primary m-1 btn-sm"><i class="fas fa-file-pdf m-1"></i>Upload</button>
+                            </form>
+                        </div>
+                    </div>
+
                     <?php if($soqm['soqm'] == 'Using'){?>
+                        <h3>You are now using System's SOQM Manual</h3>
                         <form action="/auditsystem/soqm/save" method="post">
                             <h6><b>Note:</b> If you choose not to use a specific field or section of the SOQM, please enter "N/A" in the corresponding fields. Otherwise, they will appear empty or with default values in your SOQM manual.</h6>
                             <h6><b>Note:</b> Please use <span class="text-danger"><<code>br</code>></span> at the end of your paragraphs if you want the next paragraphs to display next line. Please see the Template Below.</h6>
@@ -139,12 +138,7 @@
 
                     <?php }?>
                     <?php if($soqm['soqm'] == 'Uploaded'){?>
-                        <form action="/auditsystem/soqm/upload" enctype="multipart/form-data" method="post">
-                            <div class="col-6">
-                                <input type="file" name="soqmfile" accept=".pdf" class="form form-control btn btn-secondary" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary m-1 btn-sm"><i class="fas fa-file-pdf m-1"></i>Upload</button>
-                        </form>
+                        <h3>You are now using Your Uploaded SOQM Manual</h3>
                         <object data="<?= base_url()?>uploads/pdf/<?= decr(session()->get('firmID'))?>/soqm/<?= $soqm['soqm_data']?>" type="application/pdf" frameborder="0" width="100%" height="1000"> </object>
                     <?php }?>
                 </div>
